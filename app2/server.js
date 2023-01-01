6acf16a6b27f0cbb66dfd1931310dd8c66a49698
sam9311 <sumitmali311@gmail.com>
2023-01-01 21:22:37 +0530

    added

---
+++ app2/server.js
@@ -0,0 +1,14 @@
+var express= require("express");
+const app=express();
+var path =require("path");
+
+app.use(express.static(path.join(__dirname,'/public')));
+app.get('/',(req,res)=>{
+    if(err){
+        console.log(err)
+    }
+    res.sendFile(path.join(__dirname,'/index.html'))
+});
+
+app.listen(3000);
+console.log("app is llistening on 3000")
